00522bdb35a95cd27664a254e3a46366b24f8c43
Rubem Nakamura <rubem.nakamura@gmail.com>
2014-04-07 10:46:50 -0300

    fixup

--- recipes/default.rb
+++ recipes/default.rb
@@ -10,21 +10,13 @@ directory node['wp-cli']['dir'] do
   recursive true
 end
 
-remote_file "#{node['wp-cli']['dir']}/installer.sh" do
-  source 'http://wp-cli.org/installer.sh'
+remote_file "#{node['wp-cli']['dir']}/wp-cli.phar" do
+  source 'https://raw.github.com/wp-cli/builds/gh-pages/phar/wp-cli.phar'
   mode 0755
   action :create_if_missing
 end
 
-node['wp-cli']['bin'] = ::File.join(node['wp-cli']['dir'], 'bin', 'wp')
-
-bash 'install wp-cli' do
-  code './installer.sh'
-  cwd node['wp-cli']['dir']
-  environment 'INSTALL_DIR' => node['wp-cli']['dir'],
-              'VERSION'     => node['wp-cli']['version']
-  creates node['wp-cli']['bin']
-end
+node['wp-cli']['bin'] = ::File.join(node['wp-cli']['dir'], 'wp-cli.phar')
 
 link node['wp-cli']['link'] do
   to node['wp-cli']['bin']
